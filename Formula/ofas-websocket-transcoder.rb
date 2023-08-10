301cc3d0cf97bab5aafe22eac8be052c091180c8
Sigmar Stefansson <sigmarkarl@hotmail.com>
2023-08-10 10:07:53 +0000

    üpdate sha

--- Formula/ofas-websocket-transcoder.rb
+++ Formula/ofas-websocket-transcoder.rb
@@ -2,7 +2,7 @@ class OfasWebsocketTranscoder < Formula
     desc "TCP socket to WebSocket transcoder for NetApp Ofas Spark Applications"
     homepage "https://github.com/sigmarkarl/homebrew-ofas-websocket-transcoder"
     url "https://github.com/sigmarkarl/homebrew-ofas-websocket-transcoder/releases/download/1.0.0/ofas-websocket-transcoder"
-    sha256 "07307101278cbae1dc1102aca30f2205746aa40827cd6f869ed320fe1e296b12"
+    sha256 "4edc8d56b51a5474b5629b2154816ef1ac799911c5154db1d9229ea6ad1461ea"
     license "MIT License"
   
     # depends_on "cmake" => :build
